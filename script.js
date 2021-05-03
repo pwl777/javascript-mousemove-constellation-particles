02e410829ad35efbda3cd48f5047de9a1d570db2
Pierson Lippard <68010303+pwl777@users.noreply.github.com>
2021-05-03 08:25:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+/* ------ JavaScript - Mousemove Constellation Particles ------ */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+/* Drawing Particles with Mouse Click */
+const mouse = {
+  x: undefined, // Set position to undefined for a blank canvas.
+  y: undefined,
+};
+canvas.addEventListener("click", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    particlesArray.push(new Particle());
+  }
+});
+
+canvas.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 5; i++) {
+    particlesArray.push(new Particle());
+  }
+});
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = "hsl(" + hue + ", 100%, 50%)";
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = this.color; // Particle colour.
+    // ctx.strokeStyle = "gold";
+    // ctx.lineWidth = 5;
+    ctx.beginPath(); // Need to call ctx.beginPath first, so I can see it.
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); // The circular path is created by Math.PI * 2 = 360 degrees.
+    ctx.fill(); // This will fill the path with the colour specified above.
+    // ctx.stroke(); // Used in conjunction with strokeStyle and lineWidth above.
+  }
+}
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    for (let j = i; j < particlesArray.length; j++) {
+      const dx = particlesArray[i].x - particlesArray[j].x;
+      const dy = particlesArray[i].y - particlesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particlesArray[i].color;
+        // ctx.lineWidth = particlesArray[i].size / 10;
+        ctx.lineWidth = 0.2;
+        ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+        ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Comment this line out to paint a continuous line on the canvas.
+  // ctx.fillStyle = "rgba(0,0,0,0.02)";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  hue += 2;
+  requestAnimationFrame(animate);
+}
+animate();
